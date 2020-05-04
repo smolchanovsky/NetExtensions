274291501b6333334c4e44737186a8fed91a8940
smolchanovsky <smolchanovsky@gmail.com>
2020-05-04 15:51:48 +0500

    Add ThreadSafeRandom

---
+++ Src/NetExtensions/Randoms/ThreadSafeRandom.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NetExtensions.Randoms
+{
+	public class ThreadSafeRandom : IRandom
+	{
+		private static readonly object sync = new object();
+		private static readonly Random globalRandom = new Random();
+		
+		[ThreadStatic]
+		private static Random? threadRandomOrDefault;
+		
+		private static Random ThreadRandom
+		{
+			get
+			{
+				if (threadRandomOrDefault != null) 
+					return threadRandomOrDefault;
+			
+				int seed;
+				lock(sync)
+				{
+					seed = globalRandom.Next();
+				}
+				return threadRandomOrDefault = new Random(seed);
+			}
+		}
+
+		public int Next()
+		{
+			return ThreadRandom.Next();
+		}
+
+		public int Next(int exclusiveMax)
+		{
+			return ThreadRandom.Next(exclusiveMax);
+		}
+
+		public int Next(int inclusiveMin, int exclusiveMax)
+		{
+			return ThreadRandom.Next(inclusiveMin, exclusiveMax);
+		}
+
+		public double NextDouble()
+		{
+			return ThreadRandom.NextDouble();
+		}
+
+		public void NextBytes(byte[] buffer)
+		{
+			ThreadRandom.NextBytes(buffer);
+		}
+	}
+}
