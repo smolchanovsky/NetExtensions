3a16f2084403c92a488ff4b82c5105e42349c100
smolchanovsky <smolchanovsky@gmail.com>
2020-05-04 15:51:05 +0500

    Add Random

---
+++ Src/NetExtensions/Randoms/Random.cs
@@ -0,0 +1,22 @@
+﻿namespace NetExtensions.Randoms
+{
+	public interface IRandom
+	{
+		int Next();
+		int Next(int exclusiveMax);
+		int Next(int inclusiveMin, int exclusiveMax);
+		double NextDouble();
+		void NextBytes(byte[] buffer);
+	}
+
+	public class Random : System.Random, IRandom
+	{
+		public Random()
+		{
+		}
+
+		public Random(int seed) : base(seed)
+		{
+		}
+	}
+}
